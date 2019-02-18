e16450f6ca5e35ef98ce9137a93285462a9f15f1
Héctor Molinero <hector.molinero@stratebi.com>
2019-02-18 10:37:04 +0100

    Mejorada responsividad del formulario de búsqueda

--- src/js/elements/search-form-element.js
+++ src/js/elements/search-form-element.js
@@ -7,34 +7,48 @@ export default class SearchFormElement extends BaseElement {
 		return html`
 			<form>
 				<div class="form-group">
-					<div class="input-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-fw fa-search"></i></span>
+					<div class="btn-toolbar d-flex">
+						<div class="flex-fill mb-3 mb-md-0 mr-2">
+							<div class="input-group">
+								<input type="search" class="form-control" placeholder="Search...">
+								<div class="input-group-append">
+									<button type="button" class="btn btn-secondary"><i class="fas fa-fw fa-search"></i></button>
+								</div>
+							</div>
 						</div>
-						<input type="search" class="form-control">
-						<div class="input-group-append">
-							<button type="button" class="btn btn-outline-secondary" data-toggle="button"><i class="fac fa-fw fa-stpivot"></i></button>
-							<button type="button" class="btn btn-outline-secondary" data-toggle="button"><i class="fac fa-fw fa-streport"></i></button>
-							<button type="button" class="btn btn-outline-secondary" data-toggle="button"><i class="fac fa-fw fa-stdashboard"></i></button>
-							<button type="button" class="btn btn-outline-secondary" data-toggle="button"><i class="fac fa-fw fa-stagile"></i></button>
-							<button type="button" class="btn btn-outline-secondary" data-toggle="button"><i class="fac fa-fw fa-cde"></i></button>
+						<div class="w-100 w-md-auto">
+							<div class="btn-group">
+								<button type="button" class="btn btn-outline-secondary" data-toggle="button"><i class="fac fa-fw fa-stpivot"></i></button>
+								<button type="button" class="btn btn-outline-secondary" data-toggle="button"><i class="fac fa-fw fa-streport"></i></button>
+								<button type="button" class="btn btn-outline-secondary" data-toggle="button"><i class="fac fa-fw fa-stdashboard"></i></button>
+								<button type="button" class="btn btn-outline-secondary" data-toggle="button"><i class="fac fa-fw fa-stagile"></i></button>
+								<button type="button" class="btn btn-outline-secondary" data-toggle="button"><i class="fac fa-fw fa-cde"></i></button>
+							</div>
 						</div>
 					</div>
 				</div>
 				<div class="form-group">
-					<div class="input-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="far fa-fw fa-calendar-alt"></i></span>
+					<div class="btn-toolbar d-flex">
+						<div class="flex-fill mb-3 mb-md-0 mr-2">
+							<div class="input-group">
+								<div class="input-group-prepend">
+									<span class="input-group-text"><i class="far fa-fw fa-calendar-alt"></i></span>
+								</div>
+								<input type="date" class="form-control">
+								<div class="input-group-prepend input-group-append">
+									<span class="input-group-text"><i class="fas fa-fw fa-long-arrow-alt-right"></i></span>
+								</div>
+								<input type="date" class="form-control">
+							</div>
 						</div>
-						<input type="date" class="form-control">
-						<div class="input-group-prepend input-group-append">
-							<span class="input-group-text"><i class="fas fa-fw fa-long-arrow-alt-right"></i></span>
+						<div class="w-100 w-md-auto">
+							<div class="input-group">
+								<select class="custom-select">
+									<option selected value="creation">Creation date</option>
+									<option value="modification">Modification date</option>
+								</select>
+							</div>
 						</div>
-						<input type="date" class="form-control">
-						<select class="custom-select">
-							<option selected value="creation">Creation date</option>
-							<option value="modification">Modification date</option>
-						</select>
 					</div>
 				</div>
 			</form>
