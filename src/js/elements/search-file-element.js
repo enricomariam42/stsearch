8b03fa1e90abf5185f76af60ce6bf31a6d9da27e
Héctor Molinero <hector.molinero@stratebi.com>
2019-02-19 16:08:28 +0100

    Cambios en la estructura de elementos de archivo

--- src/js/elements/search-file-element.js
+++ src/js/elements/search-file-element.js
@@ -6,73 +6,59 @@ import SearchFileTagElement from './search-file-tag-element';
 
 export default class SearchFileElement extends BaseElement {
 	get template() {
-		let tagTemplates;
-		if (Array.isArray(this.options.tags)) {
-			tagTemplates = this.options.tags.map(options => {
-				let tag = new SearchFileTagElement(null, options);
-				return tag.template;
-			});
-		} else {
-			tagTemplates = [];
-		}
+		let tagTemplates = [
+			new SearchFileTagElement(null, {name: this.options.extension}).template
+		];
 
-		let cardFooterTemplate = html`
-			<span class="card-extra">
-				<span class="badge badge-success">Created: ${toLocaleDateTime(this.options.created)}</span>
-				<span class="badge badge-success">Modified: ${toLocaleDateTime(this.options.modified)}</span>
-			</span>
-			<span class="card-tags">${tagTemplates}</span>
-		`;
-
-		let cardBodyTemplate = html`
-			<div class="card-buttons float-sm-right">
-				<div class="btn-group">
-					<button type="button" class="btn btn-link"
-						@click=${this.editClickHandler}>
-						<i class="fas fa-edit"></i>
-					</button>
-					<button type="button" class="btn btn-link"
-						@click=${this.homeClickHandler}>
-						<i class="fas fa-home"></i>
-					</button>
-					<button type="button" class="btn btn-link"
-						@click=${this.favoriteClickHandler}>
-						<i class="fas fa-star"></i>
-					</button>
-				</div>
-			</div>
-			<h5 class="card-title pt-2">${this.options.properties['file.title']}</h5>
-			<p class="card-text">${this.options.properties['file.description']}</p>
-		`;
-
-		return this.options.properties.thumbnail
-			? html`
-				<div class="col-sm-12 col-lg-6 col-xl-4 mb-3">
-					<div class="card h-100">
-						<div class="card-body">
-							<div class="row no-gutters">
-								<div class="col-sm-2 col-lg-4 pb-2 d-flex justify-content-center align-items-start">
+		return html`
+			<div class="col-sm-12 col-lg-6 col-xl-4 mb-3">
+				<div class="card h-100">
+					<div class="card-header">
+						<div class="card-buttons m-n2 float-right">
+							<div class="btn-group">
+								<button type="button" class="btn btn-light btn-sm"
+									@click=${this.editClickHandler}>
+									<i class="fas fa-edit"></i>
+								</button>
+								<button type="button" class="btn btn-light btn-sm"
+									@click=${this.homeClickHandler}>
+									<i class="fas fa-home"></i>
+								</button>
+								<button type="button" class="btn btn-light btn-sm"
+									@click=${this.favoriteClickHandler}>
+									<i class="fas fa-star"></i>
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="card-body">
+						<div class="row no-gutters">
+							${this.options.properties.thumbnail ? html`
+								<div class="col-sm-2 col-lg-4 mb-2 d-flex justify-content-center align-items-start">
 									<img class="card-img w-auto mw-100" style="max-height: 128px" src="${this.options.properties.thumbnail}">
 								</div>
-								<div class="col-sm-10 col-lg-8 px-2">${cardBodyTemplate}</div>
-							</div>
+								<div class="col-sm-10 col-lg-8 px-2">
+									<h5 class="card-title">${this.options.properties['file.title']}</h5>
+									<p class="card-text">${this.options.properties['file.description']}</p>
+								</div>
+							` : html`
+								<div class="col-12 px-2">
+									<h5 class="card-title">${this.options.properties['file.title']}</h5>
+									<p class="card-text">${this.options.properties['file.description']}</p>
+								</div>
+							`}
 						</div>
-						<div class="card-footer">${cardFooterTemplate}</div>
+					</div>
+					<div class="card-footer">
+						<span class="card-extra">
+							<span class="badge badge-success text-wrap text-break">Created: ${toLocaleDateTime(this.options.created)}</span>
+							<span class="badge badge-success text-wrap text-break">Modified: ${toLocaleDateTime(this.options.modified)}</span>
+						</span>
+						<span class="card-tags">${tagTemplates}</span>
 					</div>
 				</div>
-			`
-			: html`
-				<div class="col-sm-12 col-lg-6 col-xl-4 mb-3">
-					<div class="card h-100">
-						<div class="card-body">
-							<div class="row no-gutters">
-								<div class="col-12 px-2">${cardBodyTemplate}</div>
-							</div>
-						</div>
-						<div class="card-footer">${cardFooterTemplate}</div>
-					</div>
-				</div>
-			`;
+			</div>
+		`;
 	}
 
 	get editClickHandler() {
--- src/js/elements/search-file-tag-element.js
+++ src/js/elements/search-file-tag-element.js
@@ -5,7 +5,7 @@ import BaseElement from './base-element';
 export default class SearchItemTagElement extends BaseElement {
 	get template() {
 		return html`
-			<a class="badge badge-primary" href="javascript:void(0)"
+			<a class="badge badge-primary text-wrap text-break" href="javascript:void(0)"
 				@click=${this.tagClickHandler}>
 				${this.options.name}
 			</a>
