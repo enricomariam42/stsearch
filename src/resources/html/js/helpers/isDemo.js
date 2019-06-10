26e0bd50587317fbd624e0cf404de22d19e7d921
Héctor Molinero <hector.molinero@stratebi.com>
2019-06-10 11:04:02 +0200

    Actualizada comprobación de demo

--- src/resources/html/js/helpers/isDemo.js
+++ src/resources/html/js/helpers/isDemo.js
@@ -1,3 +1,4 @@
 const ENV = process.env.APP_ENV;
-const DEMO_HOSTNAME = process.env.APP_DEMO_HOSTNAME || 'demo.stratebi.com';
-export default ENV === 'demo' || DEMO_HOSTNAME === window.location.hostname;
+export default ENV === 'demo'
+	|| window.location.hostname === 'demo.stratebi.com'
+	|| window.location.hostname === 'local.demo.stratebi.com';
