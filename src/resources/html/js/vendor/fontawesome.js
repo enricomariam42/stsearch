5343fb088e3d2dd850b5ff47bdc08c4de7f129be
Héctor <hector.molinero@stratebi.com>
2019-08-22 14:53:06 +0200

    Modificada carga de Font Awesome

--- src/resources/html/js/vendor/fontawesome.js
+++ src/resources/html/js/vendor/fontawesome.js
@@ -1,54 +1,53 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 
-const iconImports = [
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-regular-svg-icons/faCalendarAlt'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-regular-svg-icons/faStar'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faAngleDoubleLeft'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faAngleDoubleRight'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faAngleLeft'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faAngleRight'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faArrowLeft'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faArrowRight'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faCameraRetro'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faCaretDown'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faFolder'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faFolderOpen'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faGlobe'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faLink'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faLongArrowAltLeft'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faLongArrowAltRight'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faMapMarkedAlt'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faPencilAlt'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faSearch'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faStar'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faSync'),
-	import(/* webpackChunkName: "fa" */ '@fortawesome/free-solid-svg-icons/faTasks'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facFileAdhoc'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facFileOther'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facFilePdf'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facFilePrpt'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facFileSta'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facFileStd'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facFileUrl'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facFileWcdf'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facFileXjpivot'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facToolCde'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facToolOther'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facToolStagile'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facToolStdashboard'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facToolStpivot'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/facToolStreport'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/falGlobe'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/farHomeAlt'),
-	import(/* webpackChunkName: "fa" */ './fontawesome/fasHomeAlt')
-];
-
 (async () => {
 	const container = document.createElement('div');
 	container.style.display = 'none';
 	document.body.append(container);
 
-	const icons = await Promise.all(iconImports);
+	const icons = await Promise.all([
+		import(/* webpackMode: "eager" */ '@fortawesome/free-regular-svg-icons/faCalendarAlt'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-regular-svg-icons/faStar'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faAngleDoubleLeft'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faAngleDoubleRight'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faAngleLeft'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faAngleRight'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faArrowLeft'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faArrowRight'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faCameraRetro'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faCaretDown'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faFolder'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faFolderOpen'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faGlobe'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faLink'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faLongArrowAltLeft'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faLongArrowAltRight'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faMapMarkedAlt'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faPencilAlt'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faSearch'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faStar'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faSync'),
+		import(/* webpackMode: "eager" */ '@fortawesome/free-solid-svg-icons/faTasks'),
+		import(/* webpackMode: "eager" */ './fontawesome/facFileAdhoc'),
+		import(/* webpackMode: "eager" */ './fontawesome/facFileOther'),
+		import(/* webpackMode: "eager" */ './fontawesome/facFilePdf'),
+		import(/* webpackMode: "eager" */ './fontawesome/facFilePrpt'),
+		import(/* webpackMode: "eager" */ './fontawesome/facFileSta'),
+		import(/* webpackMode: "eager" */ './fontawesome/facFileStd'),
+		import(/* webpackMode: "eager" */ './fontawesome/facFileUrl'),
+		import(/* webpackMode: "eager" */ './fontawesome/facFileWcdf'),
+		import(/* webpackMode: "eager" */ './fontawesome/facFileXjpivot'),
+		import(/* webpackMode: "eager" */ './fontawesome/facToolCde'),
+		import(/* webpackMode: "eager" */ './fontawesome/facToolOther'),
+		import(/* webpackMode: "eager" */ './fontawesome/facToolStagile'),
+		import(/* webpackMode: "eager" */ './fontawesome/facToolStdashboard'),
+		import(/* webpackMode: "eager" */ './fontawesome/facToolStpivot'),
+		import(/* webpackMode: "eager" */ './fontawesome/facToolStreport'),
+		import(/* webpackMode: "eager" */ './fontawesome/falGlobe'),
+		import(/* webpackMode: "eager" */ './fontawesome/farHomeAlt'),
+		import(/* webpackMode: "eager" */ './fontawesome/fasHomeAlt')
+	]);
+
 	icons.forEach(icon => {
 		const symbol = document.createElement('i');
 		symbol.dataset.faSymbol = `${icon.prefix}-${icon.iconName}`;
