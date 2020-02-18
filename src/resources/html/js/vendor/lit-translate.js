4b502d55155b2d6e6057972ff424c0cf2f1970be
Héctor <hector.molinero@stratebi.com>
2020-02-18 17:55:05 +0100

    Añadido idioma catalán

--- src/resources/html/js/vendor/lit-translate.js
+++ src/resources/html/js/vendor/lit-translate.js
@@ -3,10 +3,11 @@ import { registerTranslateConfig, use } from '@appnest/lit-translate';
 import getLocale from '../helpers/biserver/getLocale';
 import en from '../../locales/en.json';
 import es from '../../locales/es.json';
+import ca from '../../locales/ca.json';
 
 const defaultLocale = 'en';
 const navigatorLocale = navigator.language.slice(0, 2);
-const availableLocales = { en, es };
+const availableLocales = { en, es, ca };
 
 registerTranslateConfig({
 	loader: locale => availableLocales[locale]
