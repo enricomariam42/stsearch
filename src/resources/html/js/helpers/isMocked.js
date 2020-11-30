275ffc09611daab33a726f95a55aeafffcdc6768
Héctor <hector.molinero@stratebi.com>
2020-11-30 10:09:53 +0100

    "APP_ENV" no es necesario

--- src/resources/html/js/helpers/isMocked.js
+++ src/resources/html/js/helpers/isMocked.js
@@ -1,6 +1,4 @@
-const ENV = process.env.APP_ENV;
-export default ENV === 'demo'
-	|| window.location.hostname === 'demo.lincebi.com'
+export default window.location.hostname === 'demo.lincebi.com'
 	|| window.location.hostname === 'demo.lincebi.localdomain'
 	|| window.location.hostname === 'demo.stratebi.com'
 	|| window.location.hostname === 'demo.stratebi.localdomain';
