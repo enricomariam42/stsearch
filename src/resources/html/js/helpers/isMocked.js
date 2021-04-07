db6f88a6df91e7df5a593852e5c580d9b2b7e7f8
Héctor <hector.molinero@stratebi.com>
2021-04-07 17:46:43 +0200

    Desactivado isMocked

--- src/resources/html/js/helpers/isMocked.js
+++ src/resources/html/js/helpers/isMocked.js
@@ -1,4 +1,1 @@
-export default window.location.hostname === 'demo.lincebi.com'
-	|| window.location.hostname === 'demo.lincebi.localdomain'
-	|| window.location.hostname === 'demo.stratebi.com'
-	|| window.location.hostname === 'demo.stratebi.localdomain';
+export default false;
