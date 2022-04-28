c5261abb21f1c7c440f24a2b33264d3c7b3e4566
Enrico Maria Carmona <enrico.carmona@asst-brianza.it>
2022-04-28 12:52:11 +0200

    Add .cda file to the searchable 'other' files

--- src/resources/html/js/helpers/biserver/extensionMap.js
+++ src/resources/html/js/helpers/biserver/extensionMap.js
@@ -8,5 +8,5 @@ export default new Map([
 	['launch.stpivot', 'xjpivot'],
 	['launch.SaikuAnalytics', 'saiku'],
 	['launch.launch-datafor', 'datafor'],
-	['other', 'csv|doc|docm|docx|dot|dotm|dotx|fodg|fodp|fods|fodt|html|mdb|odb|odc|odf|odg|odi|odm|odp|ods|odt|otc|otf|otg|oth|oti|otp|ots|ott|pdf|pot|potm|potx|ppa|ppam|pps|ppsm|ppsx|ppt|pptm|pptx|prpt|saiku|txt|url|xla|xlam|xls|xlsb|xlsm|xlsx|xlt|xltm|xltx'],
+	['other', 'cda|csv|doc|docm|docx|dot|dotm|dotx|fodg|fodp|fods|fodt|html|mdb|odb|odc|odf|odg|odi|odm|odp|ods|odt|otc|otf|otg|oth|oti|otp|ots|ott|pdf|pot|potm|potx|ppa|ppam|pps|ppsm|ppsx|ppt|pptm|pptx|prpt|saiku|txt|url|xla|xlam|xls|xlsb|xlsm|xlsx|xlt|xltm|xltx'],
 ]);
